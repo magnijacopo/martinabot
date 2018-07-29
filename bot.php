408020e497e8406f603d44a2b432d8f69db109fc
Jacopo Magni <jacopo.magni@mail.polimi.it>
2018-07-29 16:37:36 +0200

    changing logic adding inline

--- bot.php
+++ bot.php
@@ -7,12 +7,6 @@ include 'source.php';
 if($text == "/start"){
     send($cid, "Benvenuto, sono la tua commessa personale!");
 
-    $but[] = array(array("text" => "Needs Guardare", "callback_data" => "needsGuardare"),);
-    $but[] = array(array("text" => "Wants Guardare", "callback_data" => "wantsGuardare"),);
-    $but[] = array(array("text" => "Needs Guardare+Parlare", "callback_data" => "needsGuardareParlare"),);
-    $but[] = array(array("text" => "Wants Guardare+Parlare", "callback_data" => "wantsGuardareParlare"),);
-    inlineKeyboard($but, $cid, "Scegli che questionario farà la prossima persona!");
-
 
 }
 
@@ -20,4 +14,10 @@ if(callback($update)){
     if($cbdata == "ciao1"){
     send($cbid, "hai cliccato il bottone 1");
     }
+
+    $but[] = array(array("text" => "Needs Guardare", "callback_data" => "needsGuardare"),);
+    $but[] = array(array("text" => "Wants Guardare", "callback_data" => "wantsGuardare"),);
+    $but[] = array(array("text" => "Needs Guardare+Parlare", "callback_data" => "needsGuardareParlare"),);
+    $but[] = array(array("text" => "Wants Guardare+Parlare", "callback_data" => "wantsGuardareParlare"),);
+    inlineKeyboard($but, $cid, "Scegli che questionario farà la prossima persona!");
 }
